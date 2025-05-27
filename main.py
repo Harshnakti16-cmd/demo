1d326c11fe9d6277af8f043debf6a4b7dda9fa55
Harshnakti16-cmd <harshnakti0@gmail.com>
2025-05-26 22:11:13 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+
+import datetime
+
+def main():
+    print("✅ Jenkins build script is running!")
+    print("📅 Current date and time:", datetime.datetime.now())
+    print("✅ Script completed successfully.")
+
+if __name__ == "__main__":
+    main()
